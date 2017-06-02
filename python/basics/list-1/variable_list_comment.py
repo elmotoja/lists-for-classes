e69c0b9916561bbefe6fd255decade1797c18fdd
Pawel Stelmach <elmotoja@gmail.com>
2017-06-02 16:57:22 +0200

    first commit with some sample code

---
+++ python/basics/list-1/variable_list_comment.py
@@ -0,0 +1,15 @@
+# this is a comment
+
+# variable:
+some_variable = 5 # assigns a number (integer) to a variable named 'some_variable'
+variable_text = 'some text' # assigns some text to a variable named 'variable_text'
+
+# print:
+print('some text')
+print(variable_text)
+print('some text', variable_text + 'other text' + str(some_variable)) # a little more complex concatenation (merging) of strings
+
+# list:
+some_list = ['first element', variable_text, 'third element']
+
+some_list.append('fourth element') # adds another element to the end of list some_list
